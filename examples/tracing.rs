c4ff340d0971f2982bbb63da461174e4d5583ec5
Trayvon Pan <trayvonpan@gmail.com>
2024-10-30 17:36:39 +0800

    feat: add example for tracing with LogRoller and update dependencies

---
+++ examples/tracing.rs
@@ -0,0 +1,24 @@
+use logroller::{Compression, LogRollerBuilder, Rotation, RotationAge};
+use tracing_subscriber::util::SubscriberInitExt;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let appender = LogRollerBuilder::new("./logs", "tracing.log")
+        .rotation(Rotation::AgeBased(RotationAge::Minutely))
+        .compression(Compression::Gzip)
+        .build()?;
+    let (non_blocking, _guard) = tracing_appender::non_blocking(appender);
+    tracing_subscriber::fmt()
+        .with_writer(non_blocking)
+        .with_ansi(false)
+        .with_target(false)
+        .with_file(true)
+        .with_line_number(true)
+        .finish()
+        .try_init()?;
+
+    tracing::info!("This is an info message");
+    tracing::warn!("This is a warning message");
+    tracing::error!("This is an error message");
+
+    Ok(())
+}
